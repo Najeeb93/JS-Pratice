314a25668b65b4255e29d32ae3a272e7548e0838
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-03-29 19:55:03 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 
-var userInp = prompt("Enter any word");
+// var userInp = prompt("Enter any word");
 
-var firstAlpha = userInp.slice(0,1).toUpperCase();
-var baqiAlpha = userInp.slice(1).toLowerCase();
-var final = firstAlpha+baqiAlpha;
-document.write(final)
+// var firstAlpha = userInp.slice(0,1).toUpperCase();
+// var baqiAlpha = userInp.slice(1).toLowerCase();
+// var final = firstAlpha+baqiAlpha;
+// document.write(final)
+
+
+//-----Str Method -----//
+
+var arr = ["asad","saad","osama","jalal","osama"];
+
+var check = arr.indexOf("osama")
+document.write(check)

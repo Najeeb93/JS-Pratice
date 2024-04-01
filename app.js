eb1f28ea8eb74b723ac6b3043713bb420ec00f60
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-04-01 20:47:11 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -96,9 +96,24 @@
 
 // document.write(date)
 
-for(var a = 1; a < 100; a = a+10){
-    for(var b = a; b < a+10; b++){
-        document.write(b + " ")
-    }
-    document.write("<br>")
+// for(var a = 1; a < 100; a = a+10){
+//     for(var b = a; b < a+10; b++){
+//         document.write(b + " ")
+//     }
+//     document.write("<br>")
+// }
+
+var city = prompt("Enter your City");
+
+var arr = ["karachi","hyderabad","quetta"];
+city  = city.toLowerCase();
+for(var i = 0; i < arr.length; i++){
+    if(arr[i] === city){
+        alert("city has found")
 }
+else{
+    alert("city has not found")
+}
+}
+
+

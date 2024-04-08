4dfed791a87f62153648b06db5aa216a122846a0
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-04-08 21:14:36 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -190,8 +190,8 @@
 // alert("Current month: "+ currentMonth)
 
 
-var date = new Date(2016, 1, 28);
+// var date = new Date(2016, 1, 28);
 
-date.setDate(date.getDate()+2);
+// date.setDate(date.getDate()+2);
 
-document.write(date)
+// document.write(date)

2f6b1ddae2f9630903a6ebb6c6b83fd77b61c42c
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-04-15 20:48:14 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -230,3 +230,5 @@ var fruits = ["Apple","Banana","Mango","Orange","Papaya"];
 document.write(fruits[0]);
 document.write(fruits[1]);
 document.write(fruits[2]);
+document.write(fruits[3]);
+document.write(fruits[4]);

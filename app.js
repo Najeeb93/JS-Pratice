103ba850dd135a26733966269fcd4b1211c545bc
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-04-19 18:01:37 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -313,8 +313,8 @@
 
 // console.log(i)
 
-var arr = ["najeeb","ahmed","ali"];
+// var arr = ["najeeb","ahmed","ali"];
 
-for(var i = 0; i < arr.length; i++){
-    console.log(arr)
-}
+// for(var i = 0;  i < arr.length; i++){
+//     console.log(arr)
+// }

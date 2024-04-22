be9032e86e494f3fc047820bbd3ff4d11da89730
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-04-22 17:51:57 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -355,11 +355,11 @@
 // }
 
 
-// sum = 0;
-// for(var i = 1; i <= 10; i++){
-//     sum += i;
-// }
-// console.log(i)
+sum = 0;
+for(var i = 1; i <= 10; i++){
+    sum += i;
+}
+console.log(sum)
 
 // var word = prompt("Enter the word");
 

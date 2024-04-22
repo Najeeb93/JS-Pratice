8f24d591abb6dc46c1e3d502640ac92736d082c2
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-04-22 17:11:25 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -404,10 +404,7 @@ var arr = [50,51,52,,55,56,57,58];
 
 for(var i = 0; i < arr.length; i++) {
     var diff = arr[i + 1] - arr[i];
-    if(diff > 1){
-        for(var j = 1; j < diff; j++) {
-            console.log()
-            
-        }
+    for(var j = 1; j < diff; j++) {
+        console.log(diff,arr[i])
     }
 }

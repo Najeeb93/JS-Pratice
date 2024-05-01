6206f5b780794783dc2b15fe50c5aa67bbe13195
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-05-01 19:51:38 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -683,9 +683,9 @@
 // }
 // console.log(text)
 
-var fullName = "najeeb ahmed"
+// var fullName = "najeeb ahmed"
 
-var firstName = fullName.slice(0, 7)   
-var lastName = fullName.slice(7)
+// var firstName = fullName.slice(0, 7)   
+// var lastName = fullName.slice(7)
 
-console.log(lastName)
+// console.log(lastName)

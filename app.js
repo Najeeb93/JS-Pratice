f2aedd78e517e8dd4791cd0fc3e8e2d9eb819cbb
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-05-13 21:05:03 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -878,27 +878,27 @@
 // console.log(x === y)
 
 
-var day = prompt('Enter a day')
-switch(day){
-    case 'monday': 
-        console.log('work day');
-        break
-    case 'tuesday':
-        console.log('work day')
-        break
-    case 'wednesday':
-        console.log('work day')
-        break
-    case  'thusrday':
-        console.log('work day')
-        break
-    case 'friday':
-        console.log('half day')
-        break
-    case 'saturday':
-        console.log('off day')
-        break
-    default:
-        console.log('holiday')     
+// var day = prompt('Enter a day')
+// switch(day){
+//     case 'monday': 
+//         console.log('work day');
+//         break
+//     case 'tuesday':
+//         console.log('work day')
+//         break
+//     case 'wednesday':
+//         console.log('work day')
+//         break
+//     case  'thusrday':
+//         console.log('work day')
+//         break
+//     case 'friday':
+//         console.log('half day')
+//         break
+//     case 'saturday':
+//         console.log('off day')
+//         break
+//     default:
+//         console.log('holiday')     
 
-}
+// }

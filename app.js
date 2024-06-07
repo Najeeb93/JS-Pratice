f5b63ac0b732426f44869ba80b3e562ab8ec859d
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-06-07 18:11:19 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -1258,6 +1258,14 @@
 // let obj = {a:1,b:2,c:3};
 // const key = 'c';
 // console.log(obj[key])
+var a = 10;
+function abc(){
+    var a = 20;
+    console.log(a);
+}
+abc();
+console.log(a)
+
 // Q. to move all the nonzero value to the left side and the zeros value to the 
 // right side of the array
 

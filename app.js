1e16e0b4708c6547dfa7f0c5ac9ada8a68ffd0e4
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-06-08 19:55:35 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -1265,8 +1265,13 @@
 // }
 // abc();
 // console.log(a)
-
 // console.log(+'2'+3)
+var numbers = [1,2,3,4,5];
+var sum = 0;
+numbers.forEach(function(num){
+    sum += num;
+    });
+console.log(sum);
 // Q. to move all the nonzero value to the left side and the zeros value to the 
 // right side of the array
 

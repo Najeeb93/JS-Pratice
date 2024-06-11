988c14d5cbff63ea16ec1197c805f9dcfe597d04
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-06-11 18:28:09 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -1337,10 +1337,10 @@
 //     console.log('javascript')
 // }
 // name()
-function sum(a,b){
-    return a+b;
-}
-console.log(sum(1,2,3,4));
+// function sum(a,b){
+//     return a+b;
+// }
+// console.log(sum(1,2,3,4));
 // Q. to move all the nonzero value to the left side and the zeros value to the 
 // right side of the array 
 

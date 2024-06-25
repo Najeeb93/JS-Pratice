9e4dd796412e74b18c5f66f44854238a060a0e9a
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-06-25 19:47:21 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -1611,6 +1611,8 @@
 // console.log(name1 == name2); 
 // let num = [0,1,2,3,4,5];
 // console.log(num.length -1);
+let x = 4;
+console.log(x++ + ++x);     
 // // Q. to move all the nonzero value to the left side and the zeros value to the 
 // right side of the array 
 

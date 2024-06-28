60fd5559831231e96c9e6b0d624af30bdc791eb3
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-06-28 10:59:13 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -1674,8 +1674,8 @@
 // let arr = [1,2,3,4];
 // arr.splice(2,1,'a','b');
 // console.log(arr);
-const { length: len} = 'hello';
-console.log(len)   
+// const { length: len} = 'hello';
+// console.log(len)   
 // // Q. to move all the nonzero value to the left side and the zeros value to the 
 // right side of the array 
 

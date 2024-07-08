34d03449c2ec9244275bfe9d03f45b3ef0dc2774
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-07-08 18:11:43 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -1949,6 +1949,9 @@
 //     },
 // };
 // person.greet();
+let x = 10;
+let y = 20;
+console.log("total = " + x + y);
 // // Q. to move all the nonzero value to the left side and the zeros value to the 
 // right side of the array 
 

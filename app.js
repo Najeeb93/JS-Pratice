27859b850980533c098e829099e55eb55094d819
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-07-21 19:20:01 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -2316,6 +2316,10 @@
 // let count = 0;
 // const increment = () => count++;
 // console.log(increment() == 0);
+let score = 100;
+const increment = () => score++;
+console.log(increment() === score);
+
 // // Q. to move all the nonzero value to the left side and the zeros value to the 
 // right side of the array 
 

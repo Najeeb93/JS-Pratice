44acb6ed4386035f8000ed72f88997392fffba80
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-07-24 15:48:57 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -2413,11 +2413,11 @@
 //     }
 // }
 // fn1();
-async function doSomething() {
-    console.log('1');
-}
-doSomething();
-console.log('2');
+// async function doSomething() {
+//     console.log('1');
+// }
+// doSomething();
+// console.log('2');
 // // Q. to move all the nonzero value to the left side and the zeros value to the 
 // right side of the array 
 

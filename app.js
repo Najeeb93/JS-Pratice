7b395a67da2896748d66c910214c60cb01f85401
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-07-25 19:10:44 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -2430,9 +2430,9 @@
 // let arr = [1,2,3,5,6,4];
 // let result = arr[arr.length-1];
 // console.log(result);
-let arr = [1,2,3];
-let result = arr.map((ele) => ele * 2);
-console.log(result);
+// let arr = [1,2,3];
+// let result = arr.map((ele) => ele * 2);
+// console.log(result);
 // // Q. to move all the nonzero value to the left side and the zeros value to the 
 // right side of the array 
 

7ce4059b74d8831a16fa6f4797a1cc9a98561e5e
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-07-28 18:01:44 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -2494,7 +2494,7 @@
 // let b = typeof a;
 // console.log(b);
 // console.log([10] == 10);
-console.log([1,2,3] == '1,2,3');
+// console.log([1,2,3] == '1,2,3');
 // // Q. to move all the nonzero value to the left side and the zeros value to the 
 // right side of the array 
 

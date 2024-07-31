c0c3d736c82a767bba128e269b3d7c2214db1a58
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-07-31 17:33:55 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -2570,10 +2570,10 @@
 
 // let result = arr.filter(Boolean);
 // console.log(result);
-function sayHi() {
-    return (() => 0)();
-}
-console.log(typeof sayHi());
+// function sayHi() {
+//     return (() => 0)();
+// }
+// console.log(typeof sayHi());
 // // Q. to move all the nonzero value to the left side and the zeros value to the 
 // right side of the array 
 

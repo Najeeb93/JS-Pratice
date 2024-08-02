3004e615a7e66f1effd55a2fca0a5946f5a7dcac
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-08-02 19:15:51 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -2632,6 +2632,9 @@
 //     console.log(x)
 // }
 // console.log(x);
+let x = 7;
+let y = x--;
+console.log(x, y)
 // // Q. to move all the nonzero value to the left side and the zeros value to the 
 // right side of the array 
 

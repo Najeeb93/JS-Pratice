8afd91501a5bfee60a08f8b6ce02a69fbfcfa5ee
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-08-04 13:31:45 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -2685,8 +2685,8 @@
 //     console.log('Sum ='+sum);
 // }
 // show(10,20,30);
-const obj1 ={first: 20, second: 30, first: 50};
-console.log(obj1);
+// const obj1 ={first: 20, second: 30, first: 50};
+// console.log(obj1);
 // // Q. to move all the nonzero value to the left side and the zeros value to the 
 // right side of the array 
 

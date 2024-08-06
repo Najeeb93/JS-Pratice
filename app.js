acfc35fb4e6c2ab2ede8f4a53b863b2fc275b6ff
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-08-06 19:28:24 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -2732,14 +2732,14 @@
 // console.log(5 > '15'  < 5);
 // console.log(7 < '15' < 7);
 // console.log(7 < '85' > 5);
-console.log(1);
-setTimeout(() => {
-    console.log(2);
-}, 2000);
-setTimeout(() => {
-    console.log(3);
-}, 1000);
-console.log(4);
+// console.log(1);
+// setTimeout(() => {
+//     console.log(2);
+// }, 2000);
+// setTimeout(() => {
+//     console.log(3);
+// }, 1000);
+// console.log(4);
 
 
 

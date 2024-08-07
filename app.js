f6454080c9c7e6ba448e70d11ee3c0b77e0281cf
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-08-07 12:23:19 +0500

    add, to commit upload

--- app.js
+++ app.js
@@ -2784,13 +2784,13 @@
 // function abc() {
 //     console.log('3');
 // }
-const person = {
-    name: 'Ahmed',
-    age: 20,
-    name: 'Ali',
-    name: 'MR Ahmed'
-}   
-console.log(person);
+// const person = {
+//     name: 'Ahmed',
+//     age: 20,
+//     name: 'Ali',
+//     name: 'MR Ahmed'
+// }   
+// console.log(person);
 // // Q. to move all the nonzero value to the left side and the zeros value to the 
 // right side of the array 
 

f6c3cc7675379868856eaf4f6e07ea1e7a411ff6
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-08-13 18:21:44 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -3051,8 +3051,17 @@
 const arrNum = [1,2,3,4,5,8,9,10];
 const missArray = []
 const missingValue = (arr) => {
-    
+    const minValue = Math.min(...arr)
+    const maxValue = Math.max(...arr)
+    for(let i = minValue; i < maxValue; i++) {
+        if(arr.indexOf(i) < 0) {
+            missArray.push(i)
+        }
+    }
+    return missArray;
 }
+console.log(missingValue(arrNum));
+
 // // Q. to move all the nonzero value to the left side and the zeros value to the 
 // right side of the array 
 

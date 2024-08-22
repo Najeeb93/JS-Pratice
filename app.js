99b2601a1bf4aa576a81a46e578901719522c9a8
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-08-22 18:50:54 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -3375,10 +3375,10 @@
 // let b = a;
 // ++a;
 // console.log(a, b);
-let a = 5;
-let b = a++;
-let c = a + b;
-console.log(c);
+// let a = 5;
+// let b = a++;
+// let c = a + b;
+// console.log(c);
 // // Q. to move all the nonzero value to the left side and the zeros value to the 
 // right side of the array 
 

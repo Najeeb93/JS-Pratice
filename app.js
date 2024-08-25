b9962acfc75f321f4cdccaf28dbdc3339b7b4f7c
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-08-25 16:29:58 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -3421,6 +3421,12 @@
 //     console.log(y);
 // }
 // console.log(Boolean([]));
+let x = 10;
+if(true) {
+    let x = 20;
+    console.log(x);    
+}
+console.log(x);
 // // Q. to move all the nonzero value to the left side and the zeros value to the 
 // right side of the array 
 

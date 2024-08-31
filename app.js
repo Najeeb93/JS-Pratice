f20e4d54c79e28c8022f852d4e518cd66f20a9bb
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-08-31 18:15:49 +0500

    add to commit

--- app.js
+++ app.js
@@ -3474,9 +3474,9 @@
 // console.log(+'1' + 1);
 // console.log(true == 1);
 // console.log(2 < 1 == 0);
-let x = true;
-let y = !x;
-console.log(y);
+// let x = true;
+// let y = !x;
+// console.log(y);
 // // Q. to move all the nonzero value to the left side and the zeros value to the 
 // right side of the array 
 // let x = 3;
